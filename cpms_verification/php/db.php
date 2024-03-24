fdc95f44e5167a1b0f06a4e2b285b7508b5a0461
Mark Sarabia <sarabiamark0012@gmail.com>
2024-03-24 13:12:02 +0800

    delete unused files from cpms version control

--- cpms_verification/php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "37fms");
-    //Check connection
-    if(mysqli_connect_errno()){
-        echo "Failed to connect:".mysqli_connect_errno();
-    }
-?>
