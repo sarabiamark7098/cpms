cb6d799315921de7708d0a0a24bf394362bbdf5c
sarabiamark7098 <sarabiamark0012@gmail.com>
2026-03-17 10:24:28 +0800

    resolve deprecate

--- socialwork/export-data-today.php
+++ socialwork/export-data-today.php
@@ -64,7 +64,7 @@ $headers = [
     "Sub-Category", "Pantawid Beneficiary", 
     "CLient Birth Day", "Client Birth Month", "Client Birth Year", "Beneficiary Birth Day", "Beneficiary Birth Month", "Beneficiary Birth Year"
 ];
-fputcsv($output, $headers);
+fputcsv($output, $headers, ',', '"', '\\');
 
 while ($row = mysqli_fetch_assoc($result)) {
     $fullname = $user->getuserFullname($row['encoded_encoder']);
@@ -115,7 +115,7 @@ while ($row = mysqli_fetch_assoc($result)) {
         $client_bday, $client_bmonth, $client_byear, $beneficiary_bday, $beneficiary_bmonth, $beneficiary_byear
     ];
 
-    fputcsv($output, $rowData);
+    fputcsv($output, $rowData, ',', '"', '\\');
 }
 
 fclose($output);
