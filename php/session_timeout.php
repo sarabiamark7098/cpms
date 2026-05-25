f988a76b170788d1cefce5538b1b7ec4341d0ca4
sarabiamark7098 <sarabiamark0012@gmail.com>
2026-05-25 16:50:02 +0800

    update

--- php/session_timeout.php
+++ php/session_timeout.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('SESSION_TIMEOUT_SECONDS')) {
-    define('SESSION_TIMEOUT_SECONDS', 1800); // 30 minutes
+    define('SESSION_TIMEOUT_SECONDS', 7200); // 2 hours
 }
 
 if (!defined('SESSION_TIMEOUT_REDIRECT')) {
